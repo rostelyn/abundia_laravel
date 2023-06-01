45585a5da31df305dba7b8a4a28b27ce540c3326
kitt-null <jl.enriquez.0530@gmail.com>
2023-05-31 17:55:42 -0800

    homepage
    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<body>
+    <header>
+
+     <nav>
+        <a href="">Home</a>
+        <a href="">Animation</a>
+        <a href="">Transition</a>
+        <a href="">Contact Us</a>
+     </nav>
+
+@extends ('layout.app')
+
+        <h1>Welcome to Home Page</h1>
+@section('content')
+        <h1>You are on the home page</h1>
+@endsection
+</header>
+</body>
+</html>
